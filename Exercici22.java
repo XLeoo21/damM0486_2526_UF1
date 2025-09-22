e01096bb21b384150e298be37f793636dad90324
unknown <llaguna@inscastellet.cat>
2025-09-22 18:20:19 +0200

    lliurament_ex22_22_09_25

--- Exercici22.java
+++ Exercici22.java
@@ -1,33 +1,40 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Exercici22 {
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String fileName = "usertext.txt";
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 
-        boolean continuar = true;
-        while (continuar) {
-            System.out.print("Escriu una cadena (quit per sortir): ");
-            String text = scan.nextLine();
+        File f = new File("SaveFile.txt");
+        String userString = "";
+        FileOutputStream fos = new FileOutputStream(f);
 
-            if (text.equalsIgnoreCase("quit")) {
-                continuar = false;
-            } else {
-                writer.write(text);
-                writer.newLine();
+        while (!userString.equals("quit")) {
+
+            System.out.println("Enter a string to exit put quit: ");
+            userString = scan.nextLine();
+
+            if (!userString.equals("quit")) {
+                fos.write(userString.getBytes());
+                fos.write("\n".getBytes());
+            }
+
+        }
+
+        fos.close();
+
+        FileInputStream fis = new FileInputStream(f);
+
+        int b = 0;
+        while (b != -1) {
+            b = fis.read();
+            if (b != -1) {
+                System.out.print((char) b);
             }
         }
-        writer.close();
-
-        System.out.println("\nContingut del fitxer:");
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String linia = reader.readLine();
-        while (linia != null) {
-            System.out.println(linia);
-            linia = reader.readLine();
-        }
-        reader.close();
     }
 }
